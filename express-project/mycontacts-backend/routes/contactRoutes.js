a5fb2cdfa093ec6dc4b71286ee52bc21f9a27968
Deep <dr4477@srmist.edu.in>
2023-12-23 09:57:34 +0530

    feat: added error handling

--- express-project/mycontacts-backend/routes/contactRoutes.js
+++ express-project/mycontacts-backend/routes/contactRoutes.js
@@ -1,12 +1,22 @@
 const express = require("express");
-const router  = express.Router();
+const router = express.Router();
 
-const {getContacts, getContact, createContact, updateContact, deleteContact} = require("../controllers/contactController")
+const {
+  getContacts,
+  getContact,
+  createContact,
+  updateContact,
+  deleteContact,
+} = require("../controllers/contactController");
 
-router.route('/').get(getContacts);          //get all contacts
-router.route('/:id').get(getContact);        //get individual contact
-router.route('/').post(createContact);       //create a contact
-router.route('/:id').put(updateContact);     //updates a contact
-router.route('/:id').delete(deleteContact);  //delete a specific contact
+router.route("/").get(getContacts); //get all contacts
 
-module.exports = router
+router.route("/:id").get(getContact); //get individual contact
+
+router.route("/").post(createContact); //create a contact
+
+router.route("/:id").put(updateContact); //updates a contact
+
+router.route("/:id").delete(deleteContact); //delete a specific contact
+
+module.exports = router;
